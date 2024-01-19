2fc30ef3a146908b9178b403c069e292d0c43116
asnganje <abd.nganje@gmail.com>
2024-01-19 15:01:00 +0300

    setup basic react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const el = document.getElementById('root')
+const root = createRoot(el)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
