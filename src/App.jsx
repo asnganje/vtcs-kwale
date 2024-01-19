59026f73e0e87df8ae00daf1199d865343c2e120
asnganje <abd.nganje@gmail.com>
2024-01-19 16:20:21 +0300

    define login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 
 const App = () => {
   return(
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Dashboard />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
       </Routes>
     </BrowserRouter>
   )
