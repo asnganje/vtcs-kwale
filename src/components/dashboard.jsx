1f99f150ec4f2a303538bde7cebd14c86067ed1c
asnganje <abd.nganje@gmail.com>
2024-01-19 16:03:59 +0300

    modify dashboard styling

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+
+const Dashboard = () => {
+    return(
+        <main className="mx-[5%]">
+            <Header />
+            VTCs Dashboard
+        </main>
+    )
+}
+
+export default Dashboard;
