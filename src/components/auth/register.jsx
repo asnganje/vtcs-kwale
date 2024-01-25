16584b06c976b35c8bc13ac83a6b6c11b4512640
asnganje <abd.nganje@gmail.com>
2024-01-25 13:35:14 +0300

    Add logic to show successful login

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -2,17 +2,55 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 import Header from "../header";
 import Footer from "../footer";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser } from "../../redux/thunks/userThunk";
 
 const Register = () => {
+    const {registered} = useSelector((store)=>store.user)
+    
+    const dispatch = useDispatch()
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const firstNameChanger = (e) => {
+        setFirstName(e.target.value)
+    }
+
+    const lastNameChanger = (e) => {
+        setLastName(e.target.value)
+    }
+
+    const emailChanger = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const passwordChanger = (e) => {
+        setPassword(e.target.value)
+    }
+    const user = {firstName, lastName, email, password}
+    const handleUserCreate = (e) => {
+        e.preventDefault()
+        dispatch(createUser(user));
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setPassword('')
+    }
     return(
         <div className="relative mx-[5%] h-screen">
             <Header />
+            {registered && <p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
         <div className="flex items-center text-xl font-mono justify-center mt-5">
             <div className="bg-blue-100 p-5 shadow-lg rounded-md w-[70vh]">
-                <form>
+                <form onSubmit={handleUserCreate}>
                     <div className="mb-3 flex gap-5">
                         <label className="mt-1">Firstname</label>
-                        <input 
+                        <input
+                        value={firstName}
+                        onChange={firstNameChanger} 
                         type="text"
                         className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                         />
@@ -20,6 +58,8 @@ const Register = () => {
                     <div className="mb-3 flex gap-5">
                         <label className="mt-1">Lastname</label>
                         <input 
+                        value={lastName}
+                        onChange={lastNameChanger} 
                         type="text" 
                         className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
                         />
@@ -27,6 +67,8 @@ const Register = () => {
                     <div className="mb-3 flex gap-5">
                         <label className="mt-1">Email</label>
                         <input 
+                        value={email}
+                        onChange={emailChanger} 
                         type="text"
                         className="w-full ml-11 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                         />
@@ -34,6 +76,8 @@ const Register = () => {
                     <div className="mb-3 flex gap-5">
                         <label className="mt-1">Password</label>
                         <input 
+                        value={password}
+                        onChange={passwordChanger} 
                         type="text"
                         className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                         />
