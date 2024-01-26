5efd0a5bb0d5e5aceb908de20df6621e164e0955
asnganje <abd.nganje@gmail.com>
2024-01-26 20:27:15 +0300

    modify dashboard styling

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -9,24 +9,24 @@ const Dashboard = () => {
         <main className="relative h-[100%] mx-[5%]">
             <Header />
             {loggedIn && <p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
-            <div className="flex flex-col lg:flex-row justify-between h-full bg-blue-700">
-                <div className="flex flex-col w-[50%]">
-                    <h2 className="text-white w-[100%] text-4xl font-bold font-mono m-10">
+            <div className="flex flex-col lg:flex-row gap-3 justify-between h-full bg-blue-700 p-8">
+                <div className="flex flex-col w-[100%]">
+                    <h2 className="text-white w-[100%] text-4xl font-bold font-mono">
                     Cloud-based vocational training (VT) Enrolment Management
                     System.
                     </h2>
-                    <p className="text-white text-2xl w-[100% ml-10 mt-3">
+                    <p className="text-white text-2xl w-[100%] mt-3">
                     The Gateway to Streamlined
                     VT Management.
                     From a simple trainees management, 
                     to a full virtual academic platform.
                     </p>
-                    <div className="flex m-10 gap-10 w-[100%">
-                        <button className="text-white bg-orange-500 p-2 rounded-md text-xl font-bold">Book Appointment</button>
-                        <button className="text-blue-800 bg-white rounded-md text-xl font-bold p-2">Contact Us</button>
+                    <div className="flex gap-10 mt-10 w-[100%]">
+                        <button className="text-white  bg-orange-500 p-2 rounded-md text-xl font-bold">Book Appointment</button>
+                        <button className="text-blue-800  bg-white rounded-md text-xl font-bold p-2">Contact Us</button>
                     </div>
                 </div>
-                <div>
+                <div className="w-[100%]">
                     <img className="w-[100vh] rounded-lg p-8 h-[60vh] my-[5%]" src={mainImg} alt="students at vocational training" />
                 </div>
             </div>
