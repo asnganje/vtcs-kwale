b2d7078ec34a5c5596309a000ad5ae0e2279272c
asnganje <abd.nganje@gmail.com>
2024-01-26 16:05:34 +0300

    modify dashboard structure to include picture

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,14 +1,35 @@
 import { useSelector } from "react-redux";
 import Footer from "./footer";
 import Header from "./header";
+import mainImg from '../assets/vtc.png'
 
 const Dashboard = () => {
     const {loggedIn} = useSelector((store)=> store.user)
     return(
-        <main className="relative h-screen mx-[5%]">
+        <main className="relative h-[100%] mx-[5%]">
             <Header />
             {loggedIn && <p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
-            This is our story
+            <div className="flex flex-col lg:flex-row justify-between h-full bg-blue-700">
+                <div className="flex flex-col w-[50%]">
+                    <h2 className="text-white w-[100%] text-4xl font-bold font-mono m-10">
+                    Cloud-based vocational training (VT) Enrolment Management
+                    System.
+                    </h2>
+                    <p className="text-white text-2xl w-[100% ml-10 mt-3">
+                    The Gateway to Streamlined
+                    VT Management.
+                    From a simple trainees management, 
+                    to a full virtual academic platform.
+                    </p>
+                    <div className="flex m-10 gap-10 w-[100%">
+                        <button className="text-white bg-orange-500 p-2 rounded-md text-xl font-bold">Book Appointment</button>
+                        <button className="text-blue-800 bg-white rounded-md text-xl font-bold p-2">Contact Us</button>
+                    </div>
+                </div>
+                <div>
+                    <img className="w-[100vh] rounded-lg p-8 h-[60vh] my-[5%]" src={mainImg} alt="students at vocational training" />
+                </div>
+            </div>
             <Footer/>
         </main>
     )
