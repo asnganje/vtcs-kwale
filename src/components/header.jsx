da8139092126e6a00c718ec42d0f9d36932a2319
asnganje <abd.nganje@gmail.com>
2024-01-26 17:19:21 +0300

    modify header styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -34,8 +34,8 @@ const Header = () => {
                     {renderedHeaderItems}
                 </ul>
                 {<div className="p-2 flex items-center space-x-4 w-full">
-                {!headerUser && <div className="text-blue-800 p-2 text-xl"><Link to="/login"><button>Login</button></Link> <span className="text-orange-500">|</span> <Link to="/register"><button>Register</button></Link></div>}
-                {headerUser &&<p className="flex sm:flex-row gap-2 flex-col"><span className="mt-[1vh]">Welcome {headerUser}</span><Link to="/" ><button className="text-white bg-blue-700 p-1 rounded-md text-xl hover:bg-blue-400" onClick={logOutHandler}>Logout</button></Link></p>}
+                {!headerUser && <div className="text-blue-800 p-2 text-xl"><Link to="/login"><button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">Login</button></Link> <span className="text-orange-500">|</span> <Link to="/register"><button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">Register</button></Link></div>}
+                {headerUser &&<p className="flex sm:flex-row gap-2 flex-col"><span className="mt-[1vh] cursor-pointer">Welcome {headerUser}</span><Link to="/" ><button className="text-white bg-blue-700 p-1 rounded-md text-xl hover:bg-blue-400" onClick={logOutHandler}>Logout</button></Link></p>}
                 </div>}
             </div>
         </div>
