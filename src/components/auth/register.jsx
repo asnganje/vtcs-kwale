8acd67e9861f890ba7883a8a6925acf3017abe8f
asnganje <abd.nganje@gmail.com>
2024-01-27 07:55:40 +0300

    fix register module on lg screen

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -44,7 +44,7 @@ const Register = () => {
             <Header />
             {registered && <p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
             <div className="flex relative flex-col gap-3 items-center justify-center text-xl font-mono mt-5">
-                    <div className="bg-blue-100 p-5 shadow-lg rounded-md w-[100%]">
+                    <div className="bg-blue-100 p-5 shadow-lg rounded-md w-[100%] md:w-[70vh]">
                         <form onSubmit={handleUserCreate}>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Firstname</label>
