8bbea5777fa2261a799572b10fd4c9b702416d03
asnganje <abd.nganje@gmail.com>
2024-01-27 07:56:04 +0300

    add vtcs. courses and reports to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+import VTCs from "./components/VTCs";
+import Courses from "./components/Courses";
+import Reports from "./components/Reports";
 
 const App = () => {
   return(
@@ -10,6 +13,9 @@ const App = () => {
           <Route path="/" element={<Dashboard />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
+          <Route path="/vtcs" element={<VTCs />}/>
+          <Route path="/courses" element={<Courses />}/>
+          <Route path="/reports" element={<Reports />}/>
       </Routes>
     </BrowserRouter>
   )
