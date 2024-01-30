3e9cd9609dbfa13ed0bb35fa508d3a161cd385a6
asnganje <abd.nganje@gmail.com>
2024-01-30 17:13:10 +0300

    add centresReducer to store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "../slices/userSlice";
+import { centresReducer } from "../slices/centresSlice";
 const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        vtc: centresReducer
     }
 })
 
 export default store;
 export * from '../slices/userSlice'
+export * from '../slices/centresSlice'
