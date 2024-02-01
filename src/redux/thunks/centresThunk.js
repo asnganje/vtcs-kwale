52f49ad77ff594aa76e8c9bffd015a5eee8e005a
asnganje <abd.nganje@gmail.com>
2024-02-01 17:08:07 +0300

    fix base url

--- src/redux/thunks/centresThunk.js
+++ src/redux/thunks/centresThunk.js
@@ -4,8 +4,9 @@ import axios from "axios";
 const baseUrl = "https://vtc-backend-zsy1.onrender.com" || "http://localhost:3000/api/v1/vtc/centre"
 
 const createVTC = createAsyncThunk('vtc/createVTC', async (vtc)=> {
+    const url = `${baseUrl}/api/v1/vtc/centre`;
     try {
-        const response = await axios.post(baseUrl, vtc)
+        const response = await axios.post(url, vtc)
         return response.data
     } catch (error) {
         console.log(error);
@@ -14,8 +15,9 @@ const createVTC = createAsyncThunk('vtc/createVTC', async (vtc)=> {
 })
 
 const getAllVTCs = createAsyncThunk('vtc/getAllVtcs', async ()=> {
+    const url = `${baseUrl}/api/v1/vtc/centre`;
     try {
-        const response = await axios.get(baseUrl)
+        const response = await axios.get(url)
         return response.data;
     } catch (error) {
         throw new Error(`The error is ${error}`)
@@ -24,8 +26,9 @@ const getAllVTCs = createAsyncThunk('vtc/getAllVtcs', async ()=> {
 
 const removeVTC = createAsyncThunk('vtc/removeVTC', async (vtc) => {
     const {email} = vtc
+    const url = `${baseUrl}/api/v1/vtc/centre`;
     try {
-        await axios.delete(baseUrl, {data: {email}})
+        await axios.delete(url, {data: {email}})
         return email
     } catch (error) {
         throw new Error(`The error is ${error}`)
